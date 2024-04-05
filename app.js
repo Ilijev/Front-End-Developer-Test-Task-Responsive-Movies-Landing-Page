f88e94d118cc11efe7cc68dc6e687b42e6642cee
Bogdan Ilijev <bogdanilijev@yahoo.com>
2024-04-05 20:02:55 +0200

    set up api key and movie url added

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 $(function () {
-    const apiKey = "my_API_Key"; // Replace with my API key
+    const apiKey = "d96644d18099073635702aec47127ce2"; 
 
     async function fetchMovies(){
-        const url = ''
+        const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}` //TMDB API for movies 
 
         const response = await fetch(url)
         const data = await response.json()
-        return data
+        console.log(data)
+        return data.results
 
     }
-  
+    fetchMovies()
   });
   
