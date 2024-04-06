84f29fe8150bcb1d2b9b8004e3fa7f84ddb4be0d
Bogdan Ilijev <bogdanilijev@yahoo.com>
2024-04-06 12:09:49 +0200

    added a filtering functionality to the genre buttons by build a qury in the fetch function

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 $(function () {
   const apiKey = "d96644d18099073635702aec47127ce2";
+    //filter object to be able to search by each one
+  let filters = {
+    genreId: "", // genre filtering
+    searchTerm: "", // Search term filter
+  };
+
+  $('#genreBtns').on('click', '.genre-filter', function() {
+    //finding the clicked button 
+    const clickedBtn = $(this); 
+
+    filters.genreId = clickedBtn.data('genreId');
+  
+    // Refetch movies with the updated filter:
+    movieCard();
+  });
 
   async function getGenres() {
     const genresUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`; //TMDB API for genders
@@ -11,24 +26,39 @@ $(function () {
 
 
 
-  async function fetchMovies() {
+  async function fetchMovies(filters) {
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`; //TMDB API for movies
 
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.log(data);
-    return data.results;
+    console.log(filters)
+
+    let queryParams = "";
+
+    queryParams = queryParams.length ? `?${queryParams}` : "";
+    
+    if (filters.genreId) {
+      queryParams += `&with_genres=${filters.genreId}`; // Single genre ID for filtering
+    }
+
+    if (filters.searchTerm) {
+      queryParams += `&query=${filters.searchTerm}`; // Optional search term
+    }
+
+    const response = await fetch(`${url}${queryParams}`);
+    const data = await response.json(); // Parse the JSON response
+    return data.results; // Return only the movies array
+
   }
 
   async function movieCard() {
-    let movies = await fetchMovies();
-    let genres = await getGenres();
+      let genres = await getGenres();
+      let movies = await fetchMovies(filters);
 
+    //create btns for filtering through genres
     const genreBtns =  $('#genreBtns')
     genreBtns.empty();
     
-
     console.log(genres);
+    
     genres.forEach(genre=>{
         const btn =`<button class="btn btn-sm btn-light genre-filter" data-genre-id="${genre.id}">${genre.name}</button>`
         
